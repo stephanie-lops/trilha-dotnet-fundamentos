42caf1f28d1204ba573bb2d2d4356fe68b592886
stephanie-lops <stephanielopees@gmail.com>
2025-09-30 20:33:00 -0400

    mensagem do commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Stephanie was here!");
+
+DateTime currentDate = DateTime.Now;
+Console.WriteLine($"Current date and time: {currentDate}");
